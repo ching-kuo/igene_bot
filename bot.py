41c6b5f9439eaf2b1c1cce077833880cf9dee4ea
CykuTW <rft.tsumiki@gmail.com>
2017-10-09 23:55:43 +0800

    fix: check that is it google redirect or not

--- bot.py
+++ bot.py
@@ -7,6 +7,11 @@ from bs4 import BeautifulSoup
 import logging
 import re
 import requests
+try:
+    import urlparse as parse
+except ImportError:
+    from urllib import parse
+
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -22,6 +27,8 @@ def google(bot, update):
     r = requests.get('https://www.google.com/search?q='+ search)
     soup = BeautifulSoup(r.text, "html.parser")
     result = soup.find('h3', {'class': 'r'}).find('a').attrs['href']
+    if result.startswith('/'):
+        result = parse.parse_qs(parse.urlparse(result).query)['url'][0]
     update.message.reply_text(result)
 
 def girlfriend(bot, update):

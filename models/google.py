6bad5a1e495726ba19e97fd36273c09e7126f545
g0Man <aver803bath261@gmail.com>
2017-10-10 00:21:24 +0800

    use regex to process  result

--- models/google.py
+++ models/google.py
@@ -15,4 +15,13 @@ def google(bot, update):
     r = requests.get('https://www.google.com/search?q='+ search, headers)
     soup = BeautifulSoup(r.text, "html.parser")
     result = soup.find('h3', {'class': 'r'}).find('a').attrs['href']
-    update.message.reply_text(result)
+    if_http_start_regex = re.compile('^http')
+    if_http_start = if_http_start_regex.match(str(result))
+    if if_http_start == None:
+        remove_url_q_re = re.compile('^\/url\?q=')
+        remove_url_sa_re = re.compile('\&sa.+')
+        result = re.sub(remove_url_q_re, '', result)
+        result = re.sub(remove_url_sa_re, '', result)
+        update.message.reply_text(result)
+    else:
+        update.message.reply_text(result)
